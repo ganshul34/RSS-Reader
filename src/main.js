35462d971ad19d7e9ff0f5ffeff07d00b6951fff
Anshul Gupta <anshul1144.cse16@chitkara.edu.in>
2018-06-19 15:21:40 +0530

    Added initial code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import FeedList from './screens/FeedList.js';
+import FeedDetail from './screens/FeedDetail.js';
+import EntryDetail from './screens/EntryDetail.js';
+import AddFeed from './screens/AddFeed.js';
+
+import store from './store';
