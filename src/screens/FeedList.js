f94a302dcea6353294a4460089e175897fd41e64
Anshul Gupta <anshul1144.cse16@chitkara.edu.in>
2018-06-19 15:09:50 +0530

    More code git add .!

--- src/screens/FeedList.js
+++ src/screens/FeedList.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import {} from 'native-base';
+import { Container, Content, List, ListItem, Text, Icon, Button } from 'native-base';
 import { observer } from 'mobx-react/native';
-import {} from '../actions';
+import { selectFeed, removeFeed } from '../actions';
 
 @observer
 export default class FeedList extends React.Component{
-    static navigationOptions = (props) => ({
-        title: 'My Feeds',
-        headerRight: <Button 
+  static navigationOptions = (props) => ({
+    title: 'My Feeds',
+    headerRight: <Button 
       transparent
-        onPress{ () => props.navigation.navigate('Add Feed')}>
+      onPress{ () => props.navigation.navigate('Add Feed')}>
             <Icon name='add'/>
           </Button>                                  
     })
 
+  _handleFeedPress(feed) {
+    selectFeed(feed);
+    this.props.navigation.navigate(FeedDetail,{feedUrl: feed.url });
+}
+
+  render(){
+  const { feeds } = this.props.screenProps.store;
+      return(
+      <Container>
+          <Content>
+            <List>
+               {
+                feeds && feeds.map((f, i) => {
+                      return (
+                        <ListItem key={i} onPress={this._handleFeedPress.bind(this, f)}>
+                          <Text>{f.title}</Text>
+                        </ListItem>
+                      );
+                    })
+                  }
+            </List>
+          </Content>
+    </Container>
+     )
+   }
 }
