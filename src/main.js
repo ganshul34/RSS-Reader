66c1a374e79cbb1f50ee78448f8bd854cbfec496
Anshul Gupta <anshul1144.cse16@chitkara.edu.in>
2018-06-19 15:28:50 +0530

    More code

--- src/main.js
+++ src/main.js
@@ -7,3 +7,19 @@ import EntryDetail from './screens/EntryDetail.js';
 import AddFeed from './screens/AddFeed.js';
 
 import store from './store';
+
+const Navigator = StackNavigator({
+  FeedList: {screen: FeedList },
+  FeedDetail: {screen: FeedDetail },
+  EntryDetail: {screen: EntryDetail },
+  AddFeed: {screen: AddFeed }
+});
+
+export default class App extends React.Component {
+    constructor(){
+        super();
+    } 
+    render() {
+        return <Navigator screenProps={{ store }}/>;
+        }
+}
